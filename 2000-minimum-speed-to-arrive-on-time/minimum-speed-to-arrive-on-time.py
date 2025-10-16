d30257939d717dcb6053785ac27f8764bd7939d3
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-10-16 17:07:14 -0500

    Time: 1186 ms (76.76%) | Memory: 28.6 MB (79.36%) - LeetSync

--- 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
@@ -4,15 +4,13 @@ class Solution:
             return -1
         l,r=1,10**7
         res=-1
-        
         while l<=r:
             m=(l+r)//2
             time=0
             for i in range(len(dist)-1):
                 time+=math.ceil(dist[i]/m)
             time+=dist[-1]/m
-            
-            if time<=hour+1e-9:
+            if time <= hour+1e-9:
                 res=m
                 r=m-1
             else:
