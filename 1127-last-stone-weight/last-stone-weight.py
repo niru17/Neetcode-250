996cb18f76090663031021d65bcb0301ff0c3e79
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-10-25 20:10:05 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.69%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -3,17 +3,14 @@ class Solution:
         maxH=[]
         for stone in stones:
             heapq.heappush(maxH,-stone)
-        if len(maxH)==1:
-            return -maxH[0]
-        if len(maxH)==0:
-            return 0
         while len(maxH)>=2:
             y=heapq.heappop(maxH)
             x=heapq.heappop(maxH)
             if x!=y:
                 new=abs(y-x)
                 heapq.heappush(maxH,-new)
-        return -maxH[0] if maxH else 0
+        maxH.append(0)
+        return -maxH[0]
             
 
 
