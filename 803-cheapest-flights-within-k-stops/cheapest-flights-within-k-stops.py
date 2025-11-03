dc038d88cdcea6442bb6fb173dd7dafe4cb77f5c
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-11-03 14:21:22 -0600

    Time: 13 ms (58.49%) | Memory: 19.2 MB (56.31%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,18 +1,26 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        dist=[float("inf")]*n
-        dist[src]=0
-        for _ in range(k+1):
-            temp = dist.copy()
-            updated=False
-            for u,v,w in flights:
-                if dist[u]!=float("inf") and dist[u]+w<temp[v]:
-                    temp[v]=dist[u]+w
-                    updated=True
-            dist=temp
-            if not updated:
-                break
-        return -1 if dist[dst]==float("inf") else dist[dst]
+        graph=defaultdict(list)
+        for u,v,w in flights:
+            graph[u].append((v,w))
+        dist=[[float("inf")]*(k+2) for _ in range(n)]
+        dist[src][0]=0
+        pq=[(0,src,0)]
+
+        while pq:
+            cost,node,stops=heapq.heappop(pq)
+            if node==dst:
+                return cost
+            if stops==k+1:
+                continue
+            for nei, w in graph[node]:
+                new_cost=cost+w
+                if new_cost<dist[nei][stops+1]:
+                    dist[nei][stops+1]=new_cost
+                    heapq.heappush(pq,(new_cost,nei,stops+1))
+        return -1
+
+        
 
 
         
