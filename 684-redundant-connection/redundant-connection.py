99e5e75f875c0488215a6746cbfe840dab919669
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-11-03 16:09:19 -0600

    Time: 2 ms (61.08%) | Memory: 18.2 MB (56.36%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -5,7 +5,8 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n=len(edges)
         parent=[i for i in range(n+1)]
-        rank=[0]*len(parent)
+        rank=[0]*(n+1)
+
         def find(x):
             if parent[x]!=x:
                 parent[x]=find(parent[x])
@@ -16,13 +17,17 @@ class Solution:
             if px==py:
                 return False
             if rank[px]<rank[py]:
-                parent[px]=py
-            elif rank[px]>rank[py]:
-                parent[py]=px
+                parent[px]=parent[py]
+            if rank[px]>rank[py]:
+                parent[py]=parent[px]
             else:
-                parent[py]=px
+                parent[py]=parent[px]
                 rank[px]+=1
             return True
-        for u, v in edges:
-            if not union(u, v):
-                return [u, v]
+
+        for u,v in edges:
+            if not union(u,v):
+                return[u,v]
+        
+
+        
