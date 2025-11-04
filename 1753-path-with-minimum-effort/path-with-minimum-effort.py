60ec13dc749b1ccfa5e0b5d184e3b7d9751f5707
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-11-04 14:58:11 -0600

    Time: 495 ms (26.54%) | Memory: 20.7 MB (14.69%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,21 +1,20 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         m,n=len(heights),len(heights[0])
-        dist=[[float("inf")]*n for _ in range(m)]
-        dist[0][0]=0
+        dirs=[(1,0),(-1,0),(0,1),(0,-1)]
         pq=[(0,0,0)]
-        direct=[(1,0),(-1,0),(0,1),(0,-1)]
+        visited=set()
         while pq:
-            effort,r,c = heapq.heappop(pq)
-            if r==m-1 and c==n-1:
+            effort,r,c=heapq.heappop(pq)
+            if (r,c)==(m-1,n-1):
                 return effort
-            if effort>dist[r][c]:
+            if (r,c) in visited:
                 continue
-            for dr,dc in direct:
+            visited.add((r,c))
+            for dr,dc in dirs:
                 nr,nc=r+dr,c+dc
                 if 0<=nr<m and 0<=nc<n:
-                    new_effort = max(effort, abs(heights[r][c] - heights[nr][nc]))
-                    if new_effort<dist[nr][nc]:
-                        dist[nr][nc]=new_effort
-                        heapq.heappush(pq,(new_effort,nr,nc))
-        
+                    new_effort=max(effort,abs(heights[r][c]-heights[nr][nc]))
+                    heapq.heappush(pq,(new_effort,nr,nc))
+
+            
