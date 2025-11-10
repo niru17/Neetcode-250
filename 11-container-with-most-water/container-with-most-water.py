d369f47e76d9af9b1b7ead908c981cbc5d69ea86
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-11-09 19:33:59 -0600

    Time: 108 ms (28.72%) | Memory: 28.5 MB (23.86%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l,r=0,len(height)-1
-        maxP=0
+        maxArea=0
         while l<r:
             area=(r-l)*min(height[l],height[r])
-            maxP=max(area,maxP)
+            maxArea=max(area,maxArea)
             if height[l]<height[r]:
                 l+=1
             else:
                 r-=1
-        return maxP
+        return maxArea
+        
