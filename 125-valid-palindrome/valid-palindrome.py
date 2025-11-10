4a360451f5badee6fb277afadc3b9ed02582663c
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-11-09 19:41:13 -0600

    Time: 23 ms (6.04%) | Memory: 18 MB (82.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l,r=0,len(s)-1
-
         while l<r:
             while l<r and not self.alnum(s[l]):
                 l+=1
@@ -9,11 +8,11 @@ class Solution:
                 r-=1
             if s[l].lower()!=s[r].lower():
                 return False
-            l,r=l+1,r-1
+            l+=1
+            r-=1
         return True
-
-    def alnum(self,c):
-        return(ord('A')<=ord(c)<=ord('Z')or
-                ord('a')<=ord(c)<=ord('z')or
-                ord('0')<=ord(c)<=ord('9'))
-       
+    def alnum(self,char):
+        return (ord('A')<=ord(char)<=ord('Z')
+                or ord('a')<=ord(char)<=ord('z')
+                or ord('0')<=ord(char)<=ord('9'))
+        
