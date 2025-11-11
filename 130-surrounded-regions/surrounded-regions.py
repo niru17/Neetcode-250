0350bcb808eab93f9f528245a0026ecfcd8704da
Niranjana Subramanian <68531138+niru17@users.noreply.github.com>
2025-11-11 10:29:09 -0600

    Time: 3 ms (86.45%) | Memory: 21.6 MB (87.85%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -4,31 +4,32 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         rows,cols=len(board),len(board[0])
+
         def dfs(r,c):
-            if r<0 or c<0 or r>=rows or c>=cols or board[r][c]!='O':
+            if r<0 or c<0 or r>=rows or c>=cols or board[r][c]!="O":
                 return
-            board[r][c]='S'
+            board[r][c]="S"
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
         
+        for r in range(rows):
+            if board[r][0]=="O":
+                dfs(r,0)
+            if board[r][cols-1]=="O":
+                dfs(r,cols-1)
         for c in range(cols):
-            if board[0][c]=='O':
+            if board[0][c]=="O":
                 dfs(0,c)
-            if board[rows-1][c]=='O':
+            if board[rows-1][c]=="O":
                 dfs(rows-1,c)
         for r in range(rows):
-            if board[r][0]=='O':
-                dfs(r,0)
-            if board[r][cols-1]=='O':
-                dfs(r,cols-1)
-        for r in range(rows):
             for c in range(cols):
-                if board[r][c]=='O':
-                    board[r][c]='X'
-                if board[r][c]=='S':
-                    board[r][c]='O'
-                
+                if board[r][c]=="O":
+                    board[r][c]="X"
+                elif board[r][c]=="S":
+                    board[r][c]="O"
 
         
+
